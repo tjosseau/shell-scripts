f9ae83dac9c2446a00a665f6bd04d0c35195abd0
tjosseau <thomas.josseau.acc@gmail.com>
2014-01-26 01:13:46 +0100

    Updated comments

--- util/colors.sh
+++ util/colors.sh
@@ -1,20 +1,33 @@
 
-TBLACK="\033[30m"
-TRED="\033[31m"
-TGREEN="\033[32m"
-TYELLOW="\033[33m"
-TBLUE="\033[34m"
-TMAGENTA="\033[35m"
-TCYAN="\033[36m"
-TWHITE="\033[37m"
-#--------
-BBLACK="\033[40m"
-BRED="\033[41m"
-BGREEN="\033[42m"
-BYELLOW="\033[43m"
-BBLUE="\033[44m"
-BMAGENTA="\033[45m"
-BCYAN="\033[46m"
-BWHITE="\033[47m"
-#--------
-TDEFAULT="\033[00m"
+# Color variables setting
+# 
+# @author      Thomas Josseau
+# @version     1.0.0
+# @date        2014.01.26
+# @link        https://github.com/tjosseau/shell-scripts
+# 
+# @description
+# 		Shell script to add color variable shortcuts.
+
+# Text color
+black="\033[30m"
+red="\033[31m"
+green="\033[32m"
+yellow="\033[33m"
+blue="\033[34m"
+magenta="\033[35m"
+cyan="\033[36m"
+white="\033[37m"
+
+# Background color
+_black="\033[40m"
+_red="\033[41m"
+_green="\033[42m"
+_yellow="\033[43m"
+_blue="\033[44m"
+_magenta="\033[45m"
+_cyan="\033[46m"
+_white="\033[47m"
+
+# Reset all
+_def_="\033[00m"
